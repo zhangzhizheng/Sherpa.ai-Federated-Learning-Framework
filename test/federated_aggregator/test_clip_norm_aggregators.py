9ef59316aee74a32ebb42e640b99bf7c11a032ba
xehartnort <xehartnort@github.com>
2020-11-09 19:27:23 +0100

    added testfor serialize/deserialize

--- test/federated_aggregator/test_clip_norm_aggregators.py
+++ test/federated_aggregator/test_clip_norm_aggregators.py
@@ -27,9 +27,10 @@ def test_aggregated_weights_NormClip():
     assert len(aggregated_weights) == num_layers
 
 def test_aggregated_weights_WeakDP():
-    num_clients = 2
-    num_layers = 2
-    tams = [[1, 2], [2, 1]]
+    num_clients = 10
+    num_layers = 5
+    tams = [[128, 64], [64, 64], [64, 64], [64, 32], [32, 10]]
+
     np.random.seed(0)
     weights = []
     for i in range(num_clients):
@@ -200,3 +201,73 @@ def test_aggregated_weights_list_of_arrays_WeakDP():
     for i_params in range(len(clients_params[0])):
         assert np.array_equal(own_agg[i_params], aggregated_weights[i_params])
         assert aggregated_weights[i_params].shape == own_agg[i_params].shape
+
+def test_serialization_deserialization_multidimensional_3D_array():
+    num_clients = 10
+    num_rows_params = 3
+    num_cols_params = 9
+    num_k_params = 5
+
+    clients_params = []
+    for i in range(num_clients):
+        clients_params.append(np.random.rand(num_rows_params, num_cols_params, num_k_params))
+
+    avgfa = NormClipAggregator(clip=100)
+
+    serialized_params = np.array([avgfa._serialize(client)  for client in clients_params])
+    deserialized_params = np.array([avgfa._deserialize(client) for client in serialized_params])
+    
+    assert np.array_equal(deserialized_params, clients_params)
+
+def test_serialization_deserialization_multidimensional_2D_array():
+    num_clients = 10
+    num_rows_params = 3
+    num_cols_params = 9
+
+    clients_params = []
+    for i in range(num_clients):
+        clients_params.append(np.random.rand(num_rows_params, num_cols_params))
+
+    avgfa = NormClipAggregator(clip=100)
+
+    serialized_params = np.array([avgfa._serialize(client)  for client in clients_params])
+    deserialized_params = np.array([avgfa._deserialize(client) for client in serialized_params])
+    
+    assert np.array_equal(deserialized_params, clients_params)
+
+def test_serialization_deserialization():
+    num_clients = 1#10
+    num_layers = 2#5
+    tams = [[1,2],[1,1]]#[[128, 64], [64, 64], [64, 64], [64, 32], [32, 10]]
+    
+    weights = []
+    for i in range(num_clients):
+        weights.append([np.random.rand(tams[j][0], tams[j][1]) for j in range(num_layers)])
+
+    clients_params = weights
+
+    avgfa = NormClipAggregator(clip=100)
+
+    serialized_params = np.array([avgfa._serialize(client)  for client in clients_params])
+    for i, client in enumerate(serialized_params):
+        deserialized = avgfa._deserialize(client)
+        for j, arr in enumerate(deserialized):
+            assert np.array_equal(arr,clients_params[i][j])
+
+
+def test_serialization_deserialization_list_of_arrays():
+    num_clients = 2#10
+
+    clients_params = []
+    for i_client in range(num_clients):
+        clients_params.append([np.random.rand(3, 2),
+                               np.random.rand(2, 3),
+                               np.random.rand(5, 4)])
+
+    avgfa = NormClipAggregator(clip=100)
+
+    serialized_params = np.array([avgfa._serialize(client)  for client in clients_params])
+    for i, client in enumerate(serialized_params):
+        deserialized = avgfa._deserialize(client)
+        for j, arr in enumerate(deserialized):
+            assert np.array_equal(arr,clients_params[i][j])
