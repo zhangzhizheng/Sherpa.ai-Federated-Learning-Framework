96bb54634f441396c6a9dbec999c9f2041dbb217
josebummer <jantonioruiz@correo.ugr.es>
2020-11-09 18:23:13 +0100

    Configuration as parameters in DeepLearningModel class

--- shfl/model/deep_learning_model.py
+++ shfl/model/deep_learning_model.py
@@ -10,17 +10,24 @@ class DeepLearningModel(TrainableModel):
 
     # Arguments:
         model: Compiled model, ready to train
+        criterion: Loss function to apply
+        optimizer: Optimizer to apply
         batch_size: batch_size to apply
         epochs: Number of epochs
-        initialized: Indicates whether the model is initialized or not (default False)
+        metrics: Metrics for apply. List of tensorflow metrics.
     """
-    def __init__(self, model, batch_size=None, epochs=1):
+    def __init__(self, model, criterion, optimizer, batch_size=None, epochs=1, metrics=None):
         self._model = model
         self._data_shape = model.layers[0].get_input_shape_at(0)[1:]
         self._labels_shape = model.layers[-1].get_output_shape_at(0)[1:]
 
         self._batch_size = batch_size
         self._epochs = epochs
+        self._criterion = criterion
+        self._optimizer = optimizer
+        self._metrics = metrics
+
+        self._model.compile(optimizer=self._optimizer, loss=self._criterion, metrics=self._metrics)
 
     def train(self, data, labels):
         """
@@ -127,12 +134,9 @@ class DeepLearningModel(TrainableModel):
         for k, v in self.__dict__.items():
             if k == "_model":
                 model = tf.keras.models.clone_model(v)
-                config = v.optimizer.get_config()
-                cloned_opt = tf.keras.optimizers.get({"class_name": config['name'], "config": config })
-                model.compile(optimizer=cloned_opt, loss=v.loss,
-                              metrics=v.compiled_metrics._user_metrics)
                 model.set_weights(v.get_weights())
                 setattr(result, k, model)
             else:
                 setattr(result, k, copy.deepcopy(v, memo))
+        result._model.compile(optimizer=result._optimizer, loss=result._criterion, metrics=result._metrics)
         return result
