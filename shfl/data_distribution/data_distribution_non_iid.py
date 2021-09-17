7972bc13179b89e8dce166d9925bc5c69def114b
Diego Sal Díaz <d.sal@sherpa.ai>
2021-09-17 09:28:00 +0200

    Added GPL v3 headers

--- shfl/data_distribution/data_distribution_non_iid.py
+++ shfl/data_distribution/data_distribution_non_iid.py
@@ -1,3 +1,19 @@
+"""
+This file is part of Sherpa Federated Learning Framework.
+
+Sherpa Federated Learning Framework is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation version 3.
+
+Sherpa Federated Learning Framework is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 # Disable too many arguments and locals: needed in this case
 # pylint: disable=too-many-arguments, too-many-locals
 import random
